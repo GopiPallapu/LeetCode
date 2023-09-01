d172f5d92a65f0f70c958af77572e15830eae30f
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-01 14:41:54 +0530

    Time: 108 ms (61.83%) | Memory: 69.5 MB (64.51%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,11 +3,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
        unordered_map<int,int> mp;
        for(auto ele : nums)
-          mp[ele]++;
-        for(auto var : mp)
         {
-            if(var.second > 1)
+            if(mp[ele] >= 1)
                 return true;
+            mp[ele]++;
         }
         return false;
     }
