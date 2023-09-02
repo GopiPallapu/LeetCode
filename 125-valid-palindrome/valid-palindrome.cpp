fde4c23bf6f878ea86c569e9c708472535bc9838
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-02 14:44:45 +0530

    Time: 5 ms (54.62%) | Memory: 7.3 MB (83.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
+    bool help(int s , int e , vector<char>& str)
+    {
+        if(s >= e)return true;
+        if(tolower(str[s])!=tolower(str[e]))return false;
+        return help(s+1 ,e-1 , str);
+    }
     bool isPalindrome(string s) {
-        int start =0;
+         int start =0;
         int right =s.size()-1;
         while(start <= right)
         {
