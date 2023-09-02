25739d2c099cfc8e7b1a2ccdbbc13a6e5cb21a75
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-02 13:53:17 +0530

    Time: 2 ms (73.85%) | Memory: 6.1 MB (26.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
-    int fib(int n) {
+
+    int help(int n , vector<int>& dp)
+    {
         if(n == 0 || n == 1)return n;
-        return (fib(n-1)+fib(n-2));
+        if(dp[n] != -1)return dp[n];
+        int partial_ans1 = help(n-1 , dp);
+        int partial_ans2 = help(n-2 , dp);
+        dp[n] = partial_ans1 + partial_ans2;
+        return dp[n];
+    }
+    int fib(int n) {
+       vector<int> dp(n+1 , -1);
+        return help(n , dp);
     }
 };
