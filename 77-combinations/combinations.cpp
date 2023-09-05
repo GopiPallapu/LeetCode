95e0f554316ff4a3c3d6890e48d77a5d7bdb11aa
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-05 15:15:06 +0530

    Time: 126 ms (19.42%) | Memory: 111 MB (15.53%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void helper(int i , int n , int k , vector<vector<int>> &ans ,vector<int> &subset )
+    {
+        if(k == 0)
+        {
+            ans.push_back(subset);
+            return;
+        }
+        if( k > n-i+1)return;
+        if(i > n) return ;
+        subset.push_back(i);
+        helper(i+1,n,k-1,ans,subset);
+        subset.pop_back();
+        helper(i+1,n,k,ans,subset);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> subset;
+        helper(1,n,k,ans,subset);
+        return ans;
+    }
+};
