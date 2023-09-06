1ee4512e5f5357b8660f29e38e09c074602e3071
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-06 11:54:03 +0530

    Time: 31 ms (73.86%) | Memory: 61.2 MB (41.87%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool alreadyUsed(string currStr, vector<int> &selected)
+    {
+        // first step check if the curr string conains duplicate strings are not 
+        // if duplicates found , skip the string and return true
+        vector<int> self(26,0);
+        for(int i =0;i<currStr.size();i++)
+        {
+            if(self[currStr[i]-'a'] == 1) //  contains duplicates
+                return true;
+            self[currStr[i]-'a'] = 1;
+        }
+        // second step if curr string chaars are selected already 
+        for(int i =0;i<currStr.size();i++)
+        {
+            if(selected[currStr[i]-'a'] == 1) //  contains duplicates
+                return true;
+        }
+        return false;
+    }
+    int help(int i , vector<string> &arr,vector<int>&selected, int len)
+    {
+        if(i == arr.size())
+        {
+            return len;
+        }
+        if(alreadyUsed(arr[i],selected))
+        {
+            // skip curr string because it was already used means some chars are already used
+            return help(i+1,arr,selected,len);
+        }
+        else
+        {
+            // option 1 pick ith string
+            //step 1 mark chars of curr string in selected array to 1
+            for(int j =0;j<arr[i].size();j++)
+            {
+                selected[arr[i][j]-'a']=1;
+            }
+            len +=arr[i].size();
+            int op1  =help(i+1,arr,selected,len);
+            //option 2 don't pick ith string
+            for(int j =0;j<arr[i].size();j++)
+            {
+                selected[arr[i][j]-'a']=0;
+            }
+            len -=arr[i].size();
+            int op2  =help(i+1,arr,selected,len);
+
+            return max(op1,op2);
+        }
+    }
+    int maxLength(vector<string>& arr) {
+        vector<int> selected(26.0);
+        return help(0,arr,selected,0);
+    }
+};
