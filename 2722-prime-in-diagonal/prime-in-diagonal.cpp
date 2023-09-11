906848e88eee28ed6136e4ae3b66567999afb426
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-11 15:50:27 +0530

    Time: 102 ms (38.97%) | Memory: 35.9 MB (11.11%) - LeetSync

---
+++ 2722-prime-in-diagonal/prime-in-diagonal.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int largestPrimeNumber(vector<int>& nums)
+    {
+        int answer =0;
+        for( int i =0 ; i < nums.size() ; i++ )
+        {
+            int flag =0;
+            if(nums[i] == 1) continue; 
+            for(int j =2 ; j*j <= nums[i] ; j++ )
+            {
+                if(nums[i] % j == 0)
+                {
+                    flag = 1 ;
+                    break;
+                }
+            }
+            if(flag == 0)
+            {
+                answer = max(answer , nums [i]);
+            }
+        }
+        return answer;
+    }
+    int diagonalPrime(vector<vector<int>>& mat) {
+        int n = mat.size();
+        vector<int> diagonalNumbers ;
+        for(int i = 0 ;i< n; i++)
+        {
+            diagonalNumbers.push_back(mat[i][i]);
+            if( i != n-1-i)
+                diagonalNumbers.push_back(mat[i][n-1-i]);
+        }
+        return largestPrimeNumber(diagonalNumbers);
+    }
+};
