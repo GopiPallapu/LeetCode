98b0b3734e33e5ab5e95d0005938fc7547ab3785
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-11 15:34:32 +0530

    Time: 11 ms (73.76%) | Memory: 11.3 MB (40.96%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    /*
+        Approach :
+        check n is even or odd ,  if it's even add both diagonal elements
+        if it's odd , skip either primary diagonal middle or secondary diagonal middle
+
+        TC => O(n)
+        SC => O(1)
+    */
     int diagonalSum(vector<vector<int>>& mat) {
         int n = mat.size();
         int answer =0;
