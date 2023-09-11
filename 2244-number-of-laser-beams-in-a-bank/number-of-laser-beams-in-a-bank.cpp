a60924aad2268d7f75966a431a31b281052ea7ec
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-11 16:42:20 +0530

    Time: 83 ms (80.16%) | Memory: 28.8 MB (20.25%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    /*
+        Approach :
+        iterate through all arrays from bank and count no of 1's 
+        multiply it with y and add it to answer
+        then swap y to x and x to 0 for next string
+        TC => O(n* l) l = length of each string
+        SC => O(1)
+    */
     int numberOfBeams(vector<string>& bank) {
         int answer =0;
         int x =0 , y = 0;
