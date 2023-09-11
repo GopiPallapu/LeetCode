7e793327cdd4c7456e1f815557006aabc890f776
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-11 12:55:05 +0530

    Time: 7 ms (30.16%) | Memory: 8.8 MB (65.53%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.cpp
+++ 861-flipping-an-image/flipping-an-image.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     /*
         Approach :
-        TC => O(n^2)
+        TC => O(n^2) == Optimized solution is doing XOR to get bits to be flipped.
+        and swapped from first and last position TC => O(n)
         SC => O(1)
     */
     void flip(vector<int>& row , int n)
@@ -14,10 +15,19 @@ public:
     }
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
         int n =image.size();
-        for(int i =0; i< n ; i++)
+       /* for(int i =0; i< n ; i++)
         {
             reverse(image[i].begin(), image[i].end());
             flip(image[i], n);
+        }*/
+        for(int i =0;i< n;i++)
+        {
+            for(int j = 0 ;j< (n+1)/2;j++)
+            {
+                int temp  =image[i][j]^1;
+                image[i][j] = image[i][n-1-j]^1;
+                image[i][n-1-j] = temp;
+            }
         }
         return image;
     }
