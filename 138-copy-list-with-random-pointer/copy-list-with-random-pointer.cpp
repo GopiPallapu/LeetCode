9e8b2188867cb0ac987da2961894b451f0ab60ce
GopiPallapu <140641025+GopiPallapu@users.noreply.github.com>
2023-09-12 11:55:42 +0530

    Time: 7 ms (66.70%) | Memory: 11.3 MB (38.88%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -16,63 +16,23 @@ public:
 
 class Solution {
 public:
-    // TC => O(n)
-    // SC => O(1)
-    void copy(Node* head)
-    {
-        Node *temp=head , *n = head->next;
-        while(temp!=NULL)
-        {
-              Node* val = new Node(temp->val);
-             temp->next =val;
-             val->next = n;
-             temp = n;
-             if(n!=NULL)n=n->next;
-        }
-    }
-    void randommerge(Node* head)
-    {
-        Node* temp=head;
-        while(temp )
-        {   
-            if(temp->random!=NULL)
-                temp->next->random  =temp->random->next;
-            temp = temp->next->next;
-        }
-    }
-    Node* detach(Node* head)
-    {
-        Node* dummy = new Node(-1);
-        Node* tail = dummy , *temp = head;
-        while(temp)
-        {
-            tail->next = temp->next;
-            tail = tail->next;
-            temp->next = tail->next;
-            temp = temp->next;
-        }
-        return dummy->next;
-    }
     Node* copyRandomList(Node* head) {
-        if(head == NULL)return NULL;
-       /* Node *temp = head;
-        unordered_map<Node* , Node*> m;
-        while(temp!= NULL)
+        if(head == NULL)return head;
+        unordered_map<Node*, Node*> mp;
+        Node* temp = head;
+        while(temp != NULL)
         {
-            Node *newN = new Node(temp->val);
-            m[temp] = newN;
+            Node* newNode =  new Node(temp->val);
+            mp[temp] = newNode;
             temp = temp->next;
         }
         temp = head;
-        while(temp != NULL )
-        { 
-             m[temp]->next = m[temp->next];
-             m[temp]->random = m[temp->random];
-             temp = temp->next;
+        while(temp != NULL)
+        {
+            mp[temp]->next = mp[temp->next];
+            mp[temp]->random = mp[temp->random];
+            temp = temp->next;
         }
-        return m[head];*/
-        copy(head);
-        randommerge(head);
-        return detach(head);
+        return mp[head];
     }
 };
